b7bc33d2931aee098a0b7ed0addb3e31c2a21b27
Ninjakakes <48182831+Ninjakakes@users.noreply.github.com>
2023-12-19 12:20:33 -0500

    Patch vanilla rom with ips patch

---
+++ main.py
@@ -0,0 +1,32 @@
+import sys
+
+from ips import patch as ips_patch
+from typing import Union
+from pathlib import Path
+
+def ips_patch_from_file(ips_file_name: Union[str, Path], input_bytes: Union[bytes, bytearray]) -> bytearray:
+    with open(ips_file_name,"rb") as isp_file:
+        isp_data = isp_file.read()
+    return ips_patch(input_bytes, isp_data)
+
+def patch_rom_with_ips(ips_file_name: Union[str, Path], base_rom_file_name: Union[str, Path],
+                        patched_rom_file_name: Union[str,Path]) -> None:
+    with open(base_rom_file_name, "rb") as base_rom:
+        base_rom_data = base_rom.read()
+    patched_rom_data = ips_patch_from_file(ips_file_name, base_rom_data)
+
+    with open(patched_rom_file_name,"wb") as patched_rom:
+        patched_rom.write(patched_rom_data)
+
+def main(argv: list[str]) -> None:
+    rom_path = Path("roms/Super Junkoid 1.3.sfc")
+    
+    if(not rom_path.is_file()):
+        patch_rom_with_ips("Super Junkoid 1.3.ips","roms/Super Metroid (JU).sfc", rom_path)
+
+if __name__ == "__main__":
+    import time
+    t0 = time.perf_counter()
+    main(sys.argv)
+    t1 = time.perf_counter()
+    print(f"Time taken: {t1-t0}")
